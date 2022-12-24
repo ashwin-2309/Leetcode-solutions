cbe5f91cf4b6bb95969239527b167776e4549e5a
ash23090 <70055684+ash23090@users.noreply.github.com>
2022-12-24 13:34:46 +0530

    Time: 87 ms (94.48%), Space: 13.8 MB (85.08%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& a) {
-        //checking if the extension pushes to github
-        unordered_set<string> st(a.begin(),a.end());
-        int ans = 0;
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
-        while(!q.empty())
-        {
-            auto [word,steps] = q.front();
-            q.pop();
-            if(word == endWord)return steps;
-            for(int i=0;i<word.size();i++){
-                 char original = word[i];
-                 for(char ch = 'a';ch<='z';ch++)
-                 {
-                     word[i] = ch;
-                     if(st.find(word) != st.end())
-                     {
-                         st.erase(word);
-                         q.push({word,steps+1});
-                     }
-                 }
-                 word[i] = original;
+    int ladderLength(string start, string target, vector<string>& a) {
+        unordered_set<string> s(a.begin(), a.end());
+        queue<string> q;
+        q.push(start);
+        int level = 1;
+        while (!q.empty()) {
+            int size = q.size();
+            
+            // Iterate through all the words in the current level of the BFS tree
+            for (int i = 0; i < size; i++) {
+                // Get the current word from the front of the queue
+                string word = q.front();
+                q.pop();
+                
+                // If the current word is the target word, return the level
+                if (word == target) {
+                    return level;
+                }
+                
+                // Iterate through each character in the current word
+                for (int j = 0; j < word.size(); j++) {
+                    // Save the original character at the current position
+                    char original = word[j];
+                    
+                    // Try replacing the character at the current position with all the lowercase letters
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        word[j] = ch;
+                        
+                        // If the modified word is in the set of words, add it to the queue and remove it from the set
+                        if (s.count(word)) {
+                            q.push(word);
+                            s.erase(word);
+                        }
+                    }
+                    // Restore the original character in the current position
+                    word[j] = original;
+                }
             }
+            // Increment the level after processing all the words in the current level
+            level++;
         }
+        
+        // If the queue becomes empty and the target word is not found, return 0
         return 0;
     }
 };
