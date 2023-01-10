8dd5fb05f38c1f1b302dc2c29dbad180e895a606
Ashwin Kumar Singh <70055684+ash23090@users.noreply.github.com>
2023-01-10 15:15:28 +0530

    Time: 7 ms (68.12%), Space: 13.9 MB (19.56%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,49 +12,35 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // Map to store the nodes in the tree, with the x coordinate as the first key and the y coordinate
-        // as the second key. The set stores the values of the nodes.
-        map<int, map<int, multiset<int>>> nodes;
-        // Queue to store the nodes that need to be processed
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        // queue is node,{x,y}
-        // Initialize the queue with the root node and the coordinates (0, 0)
-        todo.push({root, {0, 0}});
-        // Process nodes in the queue until it is empty
-        while (!todo.empty()) {
-            // Get the next node to process and its coordinates
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int x = p.second.first, y = p.second.second;
-            // Insert the value of the node into the map
-            nodes[x][y].insert(node -> val);
-            // If the node has a left child, add it to the queue along with its coordinates
-            if (node -> left) {
-                todo.push({node -> left, {x - 1, y + 1}});
-            }
-            // If the node has a right child, add it to the queue along with its coordinates
-            if (node -> right) {
-                todo.push({node -> right, {x + 1, y + 1}});
-            }
+        map<int,map<int,multiset<int>> > m;
+        // x , y , values in sorted order
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        // Node , x, y
+         vector<vector<int>> ans;
+        if(root == NULL)return ans;
+        q.push({root,{0,0}});
+        while(!q.empty())
+        {
+            auto temp = q.front().first;
+            int x = q.front().second.first;
+            int y= q.front().second.second;
+            m[x][y].insert(temp->val);
+            q.pop();
+            if(temp->left)q.push({temp->left,{x-1,y+1}});
+            if(temp->right)q.push({temp->right,{x+1,y+1}});
         }
-        // Vector to store the values of the nodes in the tree
-        vector<vector<int>> ans;
-        // Iterate through the map and add the values of the nodes to the vector
-        for (auto p : nodes) {
-            // Vector to store the values of the nodes in a column
-            //this is int , map<int,multiset<int>>
-            
+        for(auto i : m)
+        {
             vector<int> col;
-            for (auto q : p.second) 
+            for(auto j : i.second)
             {
-                // Add the values of the nodes in the set to the column vector
-                for(auto r : q.second)
+                auto k = j.second;
+                //k is multiset
+                for(auto l = k.begin();l!=k.end();l++)
                 {
-                    col.push_back(r);
+                    col.push_back(*l);
                 }
             }
-            // Add the column vector to the answer vector
             ans.push_back(col);
         }
         return ans;
