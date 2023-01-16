08ae7311743efcfc6f4a865ec39a95c3cbbd2a4e
Ashwin Kumar Singh <70055684+ash23090@users.noreply.github.com>
2023-01-16 19:44:53 +0530

    Time: 12 ms (97.98%), Space: 8.6 MB (96.35%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        //find the path with minimum 
-        //we will use dijkstra but instead of storing distance we will store the max node in the path
-        priority_queue<pair<int,vector<int>>,vector<pair<int,vector<int>>>,greater<pair<int,vector<int>>>>  pq; 
-        int n = grid.size();
-        pq.push({grid[0][0],{0,0}});
-         vector<vector<bool>> vis(n,vector<bool>(n,false));
-        vis[0][0] = true;
-        int dx[4] = {0,0,-1,1};
-        int dy[4] = {1,-1,0,0}; 
+        // Initialize the priority queue and visited array
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid.size(), false));
+        // Push the starting point into the queue
+        pq.push({grid[0][0], 0});
+        visited[0][0] = true;
+        // Initialize the directions array
+        int dx[4] = {0, 0, -1, 1};
+        int dy[4] = {1, -1, 0, 0};
         int ans = 0;
-        while(!pq.empty())
-        {
+        // Loop until the priority queue is not empty
+        while (!pq.empty()) {
             auto node = pq.top();
-            auto cost = node.first;
-            auto x = node.second[0];
-            auto y = node.second[1];
             pq.pop();
-            if(x==n-1 and y == n-1)return cost;
-            for(int i=0;i<4;i++){
-                 int newX = x+dx[i],newY=y+dy[i];
-                 if(newX>=0 and newY >=0 and newX<n and newY<n and vis[newX][newY] == false)
-                 {
-                    vis[newX][newY] = true;
-                    int newCost = max(cost,grid[newX][newY]);
-                    pq.push({newCost,{newX,newY}});
-                 }
+            int cost = node.first;
+            int x = node.second / grid.size();
+            int y = node.second % grid.size();
+            ans = max(ans, cost);
+            if (x == grid.size() - 1 && y == grid.size() - 1) {
+                // If we have reached the destination, return the result
+                return ans;
             }
-            
+            // Explore all 4-directionally adjacent squares
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if (newX >= 0 && newY >= 0 && newX < grid.size() && newY < grid.size() && !visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    int newCost = max(cost, grid[newX][newY]);
+                    pq.push({newCost, newX * grid.size() + newY});
+                }
+            }
         }
         return ans;
     }
