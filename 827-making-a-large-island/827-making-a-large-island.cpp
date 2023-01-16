dc76836ea235cc6c858f060b896115881e8b1c4d
Ashwin Kumar Singh <70055684+ash23090@users.noreply.github.com>
2023-01-16 14:50:38 +0530

    Time: 510 ms (76.29%), Space: 149.6 MB (40.04%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cpp
+++ 827-making-a-large-island/827-making-a-large-island.cpp
@@ -1,93 +1,71 @@
-class dsu
-{
-    public:
-    vector<int> parent, size;
-    dsu(int n)
-    {
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);
-        for (int i = 0; i <= n; i++)
-        {
-            parent[i] = i;
-        }
-    }
-    int findPar(int node)
-    {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findPar(parent[node]);
-    }
-    void unionBySize(int u, int v)
-    {
-        int ulp_u = findPar(u);
-        int ulp_v = findPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (size[ulp_u] < size[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-}; 
-
 class Solution {
 public:
-    int largestIsland(vector<vector<int>>& grid) {
-        //create dsu and intialize it and connect the connected ones
-        int dx[4] = {0,0,-1,1};
-        int dy[4] = {1,-1,0,0}; 
-        int n = grid.size();
-        dsu d(n*n);
-        for(int i=0;i<n;i++){
-               for(int j=0;j<n;j++){
-                    if(grid[i][j] == 1)
-                    {
-                        for(int k=0;k<4;k++)
-                        {
-                            int newX = i+dx[k];
-                            int newY = j+dy[k];
-                            if(newX>=0 and newY>=0 and newX<n and newY<n and grid[newX][newY] == 1)
-                            {
-                                int nodeNo = i*n+j;
-                                int adjNodeNo = newX*n + newY;
-                                d.unionBySize(nodeNo,adjNodeNo);
-                            }
+    // Number of rows in the grid
+    int rows;
+    // Number of columns in the grid
+    int cols;
+    // Array to check all 4 directions
+    const int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    // Function to perform DFS
+    int dfs(vector<vector<int>>& grid, int row, int col, int label, vector<int>& islandSizes) {
+        // Base case: if the current index is out of the grid or the value is not 1
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] != 1) return 0;
+        // Mark the current cell with the current label
+        grid[row][col] = label;
+        int size = 1;
+        // Recursively call dfs on all 4 directions
+        for (auto& dir : dirs) {
+            size += dfs(grid, row + dir[0], col + dir[1], label, islandSizes);
+        }
+        // Store the size of the island in the islandSizes array at the index of the label
+        islandSizes[label] = size;
+        return size;
+   
+}
+
+int largestIsland(vector<vector<int>>& grid) {
+    rows = grid.size();
+    cols = grid[0].size();
+    int label = 2;
+    int maxSize = 0;
+
+    // Array to store the size of each island
+    vector<int> islandSizes(rows * cols + 2, 0);
+
+    // First pass: traverse the grid and mark all the connected 1's with the same label
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            if (grid[row][col] == 1) {
+                maxSize = max(maxSize, dfs(grid, row, col, label, islandSizes));
+                label++;
+            }
+        }
+    }
+
+    // Second pass: traverse the grid again and check for 0's
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            if (grid[row][col] == 0) {
+                int size = 1;
+                unordered_set<int> labels;
+                for (auto& dir : dirs) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] > 1) {
+                        int label = grid[newRow][newCol];
+                        if(labels.find(label) == labels.end()){
+                            size += islandSizes[label];
+                            labels.insert(label);
                         }
                     }
-               }
+                }
+                maxSize = max(maxSize, size);
+            }
         }
-        int mx = 0;
-        for(int i=0;i<n;i++){
-               for(int j=0;j<n;j++){
-                   if(grid[i][j] == 0)
-                   {
-                      set<int> components;
-                       for(int k=0;k<4;k++){ 
-                           int newX=i+dx[k];
-                           int newY = j+dy[k];
-                           if(newX>=0 and newY>=0 and newX<n and newY<n and grid[newX][newY] == 1)
-                           {
-                               components.insert(d.findPar(newX*n+newY));
-                           }
-                       }
-                       int sizeTotal = 0;
-                       for(auto it : components)
-                       {
-                           sizeTotal += d.size[it];
-                       }
-                       mx = max(mx,sizeTotal+1);
-                   }
-               }
-        }
-        for(int i=0;i<n*n;i++){
-             mx = max(mx,d.size[d.findPar(i)]);
-        }
-        return mx;
     }
+    return maxSize;
+}
+
 };
+
