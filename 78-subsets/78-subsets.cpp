a0257ae91a899fb986ccb4ba46d873660129b797
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-04-14 17:57:51 +0530

    Time: 9 ms (9.32%), Space: 15.8 MB (34.71%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void help( vector<vector<int>> &ans,vector<int> &nums,vector<int> curr,int startIndex)
+    {
+//         base case when startIndex >= size of the nums array
+        if(startIndex >= nums.size())
+        {
+            ans.push_back(curr);
+            return;
+        }
+//         include exclude
+//         one time include and exclude
+        
+        help(ans,nums,curr,startIndex+1);
+
+        //         include
+        
+        curr.push_back(nums[startIndex]);
+        help(ans,nums,curr,startIndex+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+//         include exclude principle
+         vector<vector<int>> ans;
+        vector<int> curr;
+        int startIndex = 0;
+        help(ans,nums,curr,startIndex);
+        return ans;
+    }
+};
