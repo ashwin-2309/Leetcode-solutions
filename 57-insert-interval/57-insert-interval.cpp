001d9a8223495563c41d5062aa430f862116245d
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-05-05 11:23:59 +0530

    Time: 12 ms (87.56%), Space: 16.9 MB (92.27%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-//         push all the elements of newInterval to intervals
-            intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        
-//         now apply non overlapping interval thingy
-         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        int n= intervals.size();
-        
-        for(int i=1;i<n;i++)
-        {
-            if(ans.back()[1] >= intervals[i][0])
-            {
-                ans.back()[1]  = max(ans.back()[1] , intervals[i][1]);
-            }
-            else{
-                ans.push_back(intervals[i]);
-            }
+        int n = intervals.size();
+        int i = 0;
+        // add all intervals that end before newInterval starts
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            i++;
         }
-        return ans;
+        // merge all overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        // insert the merged interval
+        intervals.insert(intervals.begin() + i, newInterval);
+        // remove any intervals that are completely covered by the merged interval
+        while (i + 1 < intervals.size() && intervals[i + 1][0] <= newInterval[1]) {
+            intervals.erase(intervals.begin() + i + 1);
+        }
+        // remove any intervals that overlap with the merged interval
+        while (i > 0 && intervals[i - 1][1] >= newInterval[0]) {
+            newInterval[0] = min(newInterval[0], intervals[i - 1][0]);
+            newInterval[1] = max(newInterval[1], intervals[i - 1][1]);
+            intervals.erase(intervals.begin() + i - 1);
+            i--;
+        }
+        // update the merged interval in place
+        intervals[i] = newInterval;
+        return intervals;
     }
 };
