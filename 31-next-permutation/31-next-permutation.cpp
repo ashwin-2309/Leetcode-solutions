cc7ff498d6cba4d7255dd4a51066e211c6f5d259
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-05-08 11:56:55 +0530

    Time: 3 ms (82.62%), Space: 12 MB (73.70%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-//         find the last point where a[i] < a[i+1]
-//         if there is none then return the array
-//         if there is then from that index go from the back and swap with the element that is just bigger than it
-        
+//         from scratch
+//         we have to find the last occurrence of a[i+1] > a[i]
         int index = -1;
         int n = nums.size();
+        
+//         check for the index
         for(int i=0;i<n-1;i++){
              if(nums[i] < nums[i+1])
+             {
                  index = i;
+             }
         }
-//         edge case
         if(index == -1)
         {
+//             the array is sorted in decreasing order
             reverse(nums.begin(),nums.end());
             return;
         }
-//         now the swapping part
+        
+//         now that we have found the index,traverse the array from the back and find the no.which is bigger than the nums[index]
         for(int i=n-1;i>index;i--){
-            if(nums[i] > nums[index])
-            {
+            if(nums[i] > nums[index]){
                 swap(nums[i],nums[index]);
                 break;
-            }
+            }  
         }
-//         now the sorting or reversing part
+//         now reverse the no. from index+1 to end
         reverse(nums.begin()+index+1,nums.end());
-        return;
     }
 };
