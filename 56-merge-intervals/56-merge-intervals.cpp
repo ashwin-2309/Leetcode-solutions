54c3505e6c53cf73ec4b75cd7b1a165a3c919a9f
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-05-08 12:26:03 +0530

    Time: 36 ms (83.80%), Space: 19.1 MB (71.27%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        ans.push_back(intervals[0]);
-        int n = intervals.size(); 
-        for(int i=1;i<n;i++)
-        {
-            if(intervals[i][0] <= ans.back()[1])
-            {
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
-            }
-            else{
-                ans.push_back(intervals[i]);
-            }
-        }
-        return ans;
+         vector<vector<int>> res;
+        
+         sort(intervals.begin(),intervals.end());
+        
+         res.push_back(intervals[0]);;
+        
+         int n = intervals.size();
+        
+         for(int i=1 ;i < n;i++)
+         {
+// check the back element in res and if its 2nd element is greater than the first element of intervals then merge
+             
+             int back = res.back()[1];
+             if(back >= intervals[i][0]){
+                 res.back()[1] = max(res.back()[1],intervals[i][1]);
+             }
+             else{
+                 res.push_back(intervals[i]);
+             }
+         }
+        return res;
+        
     }
 };
