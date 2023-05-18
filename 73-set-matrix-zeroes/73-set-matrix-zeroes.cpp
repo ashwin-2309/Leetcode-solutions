680cc5b29b5d6c37d896e243f1d94d0690699021
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-05-18 10:36:42 +0530

    Time: 16 ms (56.54%), Space: 13.1 MB (99.54%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -34,8 +34,67 @@ public:
         return ;
     }
     
+    void setZeroesOptimal( vector<vector<int>> &a)
+    {
+//         here we use the first row as row matrix and first col as column matrix but 
+//         a bool col0 must be used as 0,0 is intersecting(common in both)
+        
+        bool col0 = false;
+        int n = a.size();
+        int m = a[0].size(); 
+        
+        for(int i=0;i<n;i++){
+            
+//             checking for first column only
+            
+            if(a[i][0] == 0)
+            {
+                col0 = true;
+            }
+            
+               for(int j=1;j<m;j++){
+                   
+//                
+                   if(a[i][j] == 0)
+                   {
+                       a[i][0] = 0,a[0][j] = 0;
+                   }
+                   
+               }
+        }
+//         iterate over the array once again and using the first row and column update the elements
+//         iteration will start from i=1 and j=1
+        
+        for(int i=1;i<n;i++){
+               for(int j=1;j<m;j++){
+                   if(a[i][0] == 0 or a[0][j] == 0)
+                   {
+                       a[i][j] = 0;
+                   }
+               }
+        }
+        
+//         see if the first row needs to be set zero
+//         this step will be done after the previous state
+        if(a[0][0] == 0)
+        {
+            for(int j=0;j<m;j++){
+                 a[0][j] = 0;
+            }
+        }
+        
+//         check this for first column as well
+        if(col0){
+            for(int i=0;i<n;i++){
+                 a[i][0] = 0;
+            }
+        }
+        
+    }
+    
     void setZeroes(vector<vector<int>>& matrix) {
-        setZeroesBetter(matrix);
+        // setZeroesBetter(matrix);
+        setZeroesOptimal(matrix);
         return;
     }
 };
