5479872b19cb35818b13c2140e7e7808713a9f57
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-05-20 07:23:34 +0530

    Time: 4 ms (92.73%), Space: 14.8 MB (40.04%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -9,8 +9,28 @@
  * };
  */
 class Solution {
+
 public:
+    ListNode* mergeTwoListsRec(ListNode* l1,ListNode* l2)
+    {
+        if(!l1 and !l2)return NULL;
+        if(!l1)return l2;
+        if(!l2)return l1;
+        
+        if(l1->val < l2->val)
+        {
+            l1->next = mergeTwoListsRec(l1->next,l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoListsRec(l1,l2->next);
+            return l2;
+        }
+    }
+    
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        return mergeTwoListsRec(l1,l2);
+        
         if(l1 == NULL)return l2;
         if(l2 == NULL)return l1;
         
