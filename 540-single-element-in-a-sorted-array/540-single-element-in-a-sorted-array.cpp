aa8a18ba3ca0c7f250353f075ed7ef2e8468c2b3
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-06-04 12:00:25 +0530

    Time: 29 ms (56.37%), Space: 22.3 MB (44.63%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& a) {
+//         apply general purpose binary search but just check if the mid has an element
+        int start = 0,end = a.size()-2;
+//         -2 is important
+        while(start <= end)
+        {
+            int mid = start+(end-start)/2;
+            if(a[mid] == a[mid^1])
+            {
+                start = mid+1;   
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return a[start];
+    }
+};
