cddb1f4608018896a253e726840027e673f67961
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-06-04 12:01:52 +0530

    Time: 32 ms (38.84%), Space: 22.4 MB (11.61%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& a) {
 //         apply general purpose binary search but just check if the mid has an element
-        int start = 0,end = a.size()-2;
+        int start = 0,end = a.size()-1;
 //         -2 is important
-        while(start <= end)
+        while(start < end)
         {
             int mid = start+(end-start)/2;
             if(a[mid] == a[mid^1])
@@ -12,7 +12,7 @@ public:
                 start = mid+1;   
             }
             else{
-                end = mid-1;
+                end = mid;
             }
         }
         return a[start];
