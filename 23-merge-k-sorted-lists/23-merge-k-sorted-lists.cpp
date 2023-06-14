0003dc0c7d3abf40ef261f4865bc2efe3d765ed6
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-06-14 11:24:49 +0530

    Time: 34 ms (32.68%), Space: 13.7 MB (33.64%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,43 +8,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-typedef pair<int,ListNode*> pil;
+
 class Solution {
 public:
-    ListNode* heapSolution(vector<ListNode*> &lists)
-    {
-        ListNode* dummy = new ListNode();
-        ListNode* temp = dummy;
-        
-        priority_queue<pil,vector<pil>,greater<pil>> pq;
-        
-        for(auto &i : lists)
-        {
-            if(i)
-            {
-                pq.push({i->val,i});
-            }
-        }
-        
-        while(!pq.empty())
-        {
-            auto top  = pq.top();
-            pq.pop();
-            
-            auto ptr = top.second;
-            
-            temp->next = ptr;
-            temp = temp->next;
-            
-            if(ptr->next)
-            {
-                pq.push({ptr->next->val,ptr->next});
-            }
-        }
-        return dummy->next;
-    }
+    // we define pair as pi
+    #define pi pair<int, ListNode* >
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        return heapSolution(lists);
+        int k = lists.size(); // taking the size of the linked list
+        
+        if(k == 0) // if no linked list is present
+            return NULL; // simply return null
+        
+        priority_queue<pi, vector<pi>, greater<pi>> minh; // making priority queue
+        
+        for(int i = 0; i < k; i++) // traverse from the whole array 
+        {
+            ListNode* curr_list = lists[i]; // extracting current linked list
+            
+            if(curr_list != NULL) // if element present in the linked list
+            {
+                minh.push({curr_list -> val, curr_list}); // push into min heap
+            }
+        }
+        
+        // this does not gurantee that k is zero, 
+        // suppose an array like this [[],[],[],],here k = 3 and size of array is 0
+        if(minh.size() == 0) // if their is no element i.e zero element
+            return NULL;
+        
+        ListNode* head = new ListNode(); // can also be called as dummy
+        ListNode* curr = head; // make a pointer pointing to head
+        
+        while(minh.empty() == false) // adding further most elements to min heap
+        {
+            pair<int, ListNode*> temp = minh.top(); // extracting top pair
+            minh.pop(); // pop that pair
+            
+            if(temp.second -> next != NULL) // if elements still remaining in the linked list then push them
+            {
+                minh.push({temp.second -> next -> val, temp.second -> next});
+            }
+            
+            curr -> next = temp.second;
+            curr = curr -> next;
+        }
+        
+        curr -> next = NULL; 
+        head = head -> next; // move head, which is actually containg the list
+        
+        return head; // return head
     }
 };
