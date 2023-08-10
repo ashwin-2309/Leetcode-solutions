95164ba839ee44d378ef75a5657eea30a5b2c000
Ashwin Kumar Singh <70055684+ashwin-2309@users.noreply.github.com>
2023-08-10 11:49:20 +0530

    Time: 5 ms (39.92%), Space: 11.5 MB (82.96%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -43,17 +43,20 @@
 class Solution {
 public:
     void generateParenthesisHelper(vector<string> &result, string &current, int openCount, int closeCount, int n) {
+        // Base case: If the length of the current string is equal to 2*n, we have a valid combination
         if (current.length() == 2 * n) {
             result.push_back(current);
             return;
         }
         
+        // Try adding an opening parenthesis if the count of open parentheses is less than n
         if (openCount < n) {
             current.push_back('(');
             generateParenthesisHelper(result, current, openCount + 1, closeCount, n);
             current.pop_back();
         }
         
+        // Try adding a closing parenthesis if the count of close parentheses is less than open parentheses
         if (closeCount < openCount) {
             current.push_back(')');
             generateParenthesisHelper(result, current, openCount, closeCount + 1, n);
@@ -64,7 +67,11 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
         string current;
+        
+        // Start the backtracking process with initial counts of open and close parentheses as 0
         generateParenthesisHelper(result, current, 0, 0, n);
+        
         return result;
     }
 };
+
